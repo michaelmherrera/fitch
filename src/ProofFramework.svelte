01ea256448dcd73854971268b683eda484fc2186
Michael Herrera <52046844+michaelmherrera@users.noreply.github.com>
2021-12-11 23:29:25 -0800

    Intermediate

--- src/ProofFramework.svelte
+++ src/ProofFramework.svelte
@@ -1,11 +1,25 @@
-<div class="sub-proof" style="height: fit-content">
-    <div>
-        <div class="premise-outer">
-            <div class="premise-inner"><slot name="premise"></slot></div>
-        </div>
-        <div class="proof-body">
+<script>
+    export let length
+	import Katex from "./Katex.svelte";
+</script>
+
+<main>
+
+	<div class="proof-framework">
+		<div>#</div>
+		<div>Statement</div>
+		<div>Justification</div>
+		<div class="num-col">
+            {#each {length: length} as _, i}
+                <div><Katex math={`${i+1}`}/></div>
+            {/each}
+		</div>
+		<div name="statement-col">
             <slot name="body"></slot>
-        </div>
-    </div>
- </div>
+		</div>
+		<div class="justification-col">
+            <slot name="justification"></slot>
+		</div>
+	</div>
+</main>
 
