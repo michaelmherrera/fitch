6553dd152f9b954b80dcc5484fa30082f037139f
Michael Herrera <52046844+michaelmherrera@users.noreply.github.com>
2021-12-19 17:16:07 -0800

    Fixed labels

--- src/InputSubproof.svelte
+++ src/InputSubproof.svelte
@@ -1,5 +1,4 @@
 <script>
-    import { writable } from "svelte/store";
     import SubproofFramework from "./SubproofFramework.svelte";
     export let subproof;
 
@@ -17,13 +16,13 @@
 <SubproofFramework>
     <svelte:fragment slot="premise">
         {#each subproof.premise as prem}
-            <div><input bind:value={prem} /></div>
+            <div><input bind:value={prem} class="expression-input"/></div>
         {/each}
     </svelte:fragment>
     <svelte:fragment slot="body">
         {#each subproof.body as elem}
             {#if typeof elem === "string"}
-                <div><input bind:value={elem} /></div>
+                <div><input bind:value={elem} class="expression-input"/></div>
             {:else}
                 <svelte:self bind:subproof={elem}/>
             {/if}
